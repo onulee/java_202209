5c9af0c77101b2fa6a5302097858f64bb9203858
SanghunOh <otter.oh@gmail.com>
2022-09-26 16:45:42 +0900

    commit

--- d0922_sms/stupjt/member/urls.py
+++ d0922_sms/stupjt/member/urls.py
@@ -3,10 +3,25 @@ from . import views
 
 app_name = "member"
 urlpatterns = [
+    # 회원전체리스트
+    path('memList/',views.memList,name="memList"),
+    # 회원상세페이지
+    path('<str:m_id>/memView/',views.memView,name="memView"),
+    # 회원수정페이지
+    path('<str:m_id>/memUpdate/',views.memUpdate,name="memUpdate"),
+    # 회원수정저장
+    path('memUpdateOk/',views.memUpdateOk,name="memUpdateOk"),
+    # 회원정보삭제
+    path('<str:m_id>/memDelete/',views.memDelete,name="memDelete"),
+    
     # 회원가입 호출
     path('memWrite/',views.memWrite,name="memWrite"),
     # 회원정보 저장
     path('memWriteOk/',views.memWriteOk,name='memWriteOk'),
     # 로그인
     path('login/', views.login, name='login'),
+    # 로그인체크
+    path('loginOk/', views.loginOk, name="loginOk"),
+    # 로그아웃
+    path('logout/', views.logout, name='logout'),
 ]
--- d0922_sms/stupjt/member/views.py
+++ d0922_sms/stupjt/member/views.py
@@ -1,12 +1,76 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from member.models import Member
+
+# 회원전체리스트
+def memList(request):
+    qs = Member.objects.all()
+    count = qs.count()
+    context={'memList':qs,'count':count}
+    return render(request,'memList.html',context)
+
+# 회원상세페이지
+def memView(request,m_id):
+    qs = Member.objects.get(m_id=m_id)
+    context={'mem':qs}
+    return render(request,'memView.html',context)
+
+# 회원수정페이지
+def memUpdate(request,m_id):
+    qs = Member.objects.get(m_id=m_id)
+    context={'mem':qs}
+    return render(request,'memUpdate.html',context)
+
+# 회원수정저장
+def memUpdateOk(request):
+    m_id = request.POST.get('id')
+    m_pw = request.POST.get('pw')
+    m_gender = request.POST.get('gender')
+    m_hobby = request.POST.getlist('hobby')
+    print('memWrite : ', m_hobby)
+    hobby = ','.join(m_hobby)  # list타입 -> str
+    print(hobby)
+    qs = Member.objects.get(m_id=m_id)
+    qs.m_pw = m_pw
+    qs.m_gender = m_gender
+    qs.m_hobby = hobby
+    qs.save()
+    return HttpResponseRedirect(reverse('member:memList'))
+
+# 회원정보삭제
+def memDelete(request,m_id):
+    qs = Member.objects.get(m_id=m_id)
+    qs.delete()
+    return HttpResponseRedirect(reverse('member:memList'))
+
 
 # login
 def login(request):
     return render(request,'login.html')
 
+# loginOk
+def loginOk(request):
+    id = request.POST.get('id')
+    pw = request.POST.get("pw")
+    print("member login : ",id)
+    try:
+        qs = Member.objects.get(m_id=id,m_pw=pw)
+        # 섹션에 저장
+        request.session['session_id'] = qs.m_id
+        request.session['session_gender'] = qs.m_gender
+        print("session end" )
+        # id,pw가 틀린경우, 없는 경우
+        return redirect("/")
+    except:
+        context={'msg':"아이디 또는 패스워드가 일치하지 않습니다.\\n다시 로그인 바랍니다."}
+        return render(request,'login.html',context)
+
+# logout
+def logout(request):
+    # session 모두 삭제
+    request.session.clear()
+    return redirect("/")
 
 # 회원정보페이지 호출
 def memWrite(request):
@@ -24,4 +88,4 @@ def memWriteOk(request):
     qs = Member(m_id=m_id,m_pw=m_pw,m_gender=m_gender,m_hobby=hobby)
     qs.save()
     
-    return HttpResponseRedirect(reverse('index'))
+    return HttpResponseRedirect(reverse('member:memList'))
